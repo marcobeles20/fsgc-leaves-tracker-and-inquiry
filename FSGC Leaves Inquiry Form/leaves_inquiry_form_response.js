1239bc369addfd8159f52dcfc9274679f9d6cb4c
Marc Jefferson Obeles <marcobeles20@gmail.com>
2024-05-17 21:29:44 +0800

    Creates new files for globally used functions

--- FSGC Leaves Inquiry Form/leaves_inquiry_form_response.js
+++ FSGC Leaves Inquiry Form/leaves_inquiry_form_response.js
@@ -40,16 +40,3 @@ function create_leaves_inquiry_form_response_trigger()
     .onFormSubmit()
     .create();
 }
-
-function delete_trigger(trigger_name)
-{
-  const triggers = ScriptApp.getProjectTriggers();
-
-  for(const i in triggers)
-  {
-    if(triggers[i].getHandlerFunction() != trigger_name)
-      continue;
-
-    ScriptApp.deleteTrigger(triggers[i]);
-  }
-}
